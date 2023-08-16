d1e401495360e03b9d7282e7fed9321192af8367
abhinav reddy <abhinavreddy_k@srmap.edu.in>
2023-08-16 15:03:35 +0530

    Add existing project files to Git

---
+++ ClassTeacher/Includes/footer.php
@@ -0,0 +1,8 @@
+<footer class="sticky-footer bg-white">
+        <div class="container my-auto">
+          <div class="copyright text-center my-auto">
+            <span> &copy; <script> document.write("Group-2 CSE - H"); </script> 
+            </span>
+          </div>
+        </div>
+      </footer>
